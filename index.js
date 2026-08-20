0bbf8b3ed94c3d1eda6b8a6043ad416c7541facc
Daniel Lage <daniellc3221@gmail.com>
2026-08-20 20:56:32 -0300

    feat: rota inexistente responde com status 404

--- index.js
+++ index.js
@@ -32,6 +32,9 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
     return res.end(`Olá, ${nome}!`);
   }
+
+  res.writeHead(404);
+  res.end();
 });
 
 server.listen(PORT, () => console.log(`Servidor em http://localhost:${PORT}`));
